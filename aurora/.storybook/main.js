0c4212ecb000bd790fe90c5b25007a86b3a8fdb0
Amanda Simonds <amandasimonds9@gmail.com>
2023-05-02 12:48:26 -0400

    global styles from the scss file implemented, switched to light theme

--- aurora/.storybook/main.js
+++ aurora/.storybook/main.js
@@ -1,16 +1,16 @@
 module.exports = {
-  core: {
-    builder: 'webpack5',
-  },
-  "stories": [
-    "*.stories.ts",
-    "../stories/**/*.stories.mdx",
-    "../stories/**/*.stories.@(js|jsx|ts|tsx)"
-  ],
-  "addons": [
-    "@storybook/addon-links",
-    "@storybook/addon-essentials",
-    "@storybook/addon-interactions"
-  ],
-  "framework": "@storybook/angular"
+    core: {
+        builder: 'webpack5',
+    },
+    stories: [
+        '*.stories.ts',
+        '../stories/**/*.stories.mdx',
+        '../stories/**/*.stories.@(js|jsx|ts|tsx)',
+    ],
+    addons: [
+        '@storybook/addon-links',
+        '@storybook/addon-essentials',
+        '@storybook/addon-interactions',
+    ],
+    framework: '@storybook/angular',
 }
